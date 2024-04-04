053c6e0e6dd0d496bb06c2680db55375c980ebfa
Nichole Williams <nicci@nicciwill.com>
2024-04-04 14:50:10 -0400

    moved index.js to root

---
+++ index.js
@@ -0,0 +1,13 @@
+import * as styles from './src/shared/styles';
+import * as animation from './src/shared/animation';
+import * as icons from './src/shared/icons';
+import * as global from './src/shared/global';
+
+export { styles, animation, icons, global };
+
+export * from './src/Avatar';
+export * from './src/Badge';
+export * from './src/Button';
+export * from './src/Icon';
+export * from './src/Link';
+export * from './src/LinkWrapper';
