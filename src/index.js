a72ecc91fd2350566b2d86e8c28efcc1012c7e0d
Nichole Williams <nicci@nicciwill.com>
2024-04-04 17:33:24 -0400

    slop

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import * as styles from './shared/styles.js';
-import * as animation from './shared/animation.js';
-import * as icons from './shared/icons.js';
-import * as global from './shared/global.js';
+// import * as styles from './shared/styles.js';
+// import * as animation from './shared/animation.js';
+// import * as icons from './shared/icons.js';
+// import * as global from './shared/global.js';
 
-export { styles, animation, icons, global };
+// export { styles, animation, icons, global };
 
 export * from './Avatar';
 export * from './Badge';
