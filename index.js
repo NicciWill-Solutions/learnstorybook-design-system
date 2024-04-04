7e950136a6f3a042df2d479e5c38329d7d3ba0ba
Nichole Williams <nicci@nicciwill.com>
2024-04-04 15:16:09 -0400

    refactored the main exports

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ import * as global from './src/shared/global';
 
 export { styles, animation, icons, global };
 
-export * from './src/Avatar';
-export * from './src/Badge';
-export * from './src/Button';
-export * from './src/Icon';
-export * from './src/Link';
-export * from './src/LinkWrapper';
+// export * from './src/Avatar';
+// export * from './src/Badge';
+// export * from './src/Button';
+// export * from './src/Icon';
+// export * from './src/Link';
+// export * from './src/LinkWrapper';
+
+export { Avatar } from './src/Avatar';
+export { Badge } from './src/Badge';
+export { Button } from './src/Button';
+export { Icon } from './src/Icon';
+export { Link } from './src/Link';
+export { LinkWrapper } from './src/LinkWrapper';
